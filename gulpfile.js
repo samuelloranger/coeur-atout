facd35cd9321f34fb603f2725f2f7ec7e49d82ea
Michel Rouleau <mrouleau@cegep-ste-foy.qc.ca>
2019-08-29 12:45:33 -0400

    Mise à jour du fichier gulpfile pour la version 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,38 @@
-'use strict';
-/*
-* Références:
-* principale https://gist.github.com/feliperoberto/9793674
-* et la doc des extensions gulp
-* */
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourceMaps = require('gulp-sourcemaps');
+const autoPrefixer = require('gulp-autoprefixer');
+const browserSync = require("browser-sync").create();
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
-var browserSync = require('browser-sync');
-
-gulp.task('sass', function () {
-    gulp.src('src/scss/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'expanded', includePaths: ["scss/"]}))
-        .on('error', sass.logError)
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
+function styles(cb) {
+    return gulp.src('./scss/*.scss')
+        .pipe(sourceMaps.init())
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoPrefixer({
+            /*browsers: ['last 2 versions'],*/
             cascade: false
         }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('assets/css'));
-});
+        .pipe(sourceMaps.write())
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+}
 
-gulp.task('browser-sync', function () {
-    browserSync.init(["assets/css/*.css", "./*.html", "assets/js/!*.js"], {
-        watch:true,
-        server: {
+function watch(cb) {
+    browserSync.init({
+        server:{
             baseDir: "./"
         }
     });
-});
+    gulp.watch('./scss/*.scss', gulp.series('styles'));
+    cb();
+}
 
+function defaut(cb){
+    console.log("allo");
+    // place code for your default task here
+    cb();
+}
 
-// Tâche par défaut
-gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("src/scss/**/*.scss", ['sass']);
-});
+exports.default=defaut;
+exports.styles=styles;
+exports.watch=watch;
